bb9d243b91871038f47aadd29f7792d460cd410d
Jiahe Ding <hzdingjiahe@gmail.com>
2017-04-24 12:07:29 +0800

    从VSTS迁移到Github。

---
+++ Assets/Script/EndDragAndShowButton.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndDragAndShowButton : MonoBehaviour
+{
+    public Book book;
+    public GameObject Master;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnMouseDragEndAndShowButton()
+    {
+
+        StartCoroutine(WaitAndShowLog());
+    }
+
+    IEnumerator WaitAndShowLog()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Debug.Log("show!" + book.currentPage.ToString());
+        Master.SetActive(true);
+    }
+
+    public void OnMouseDragBeginAndHideButton()
+    {
+        Master.SetActive(false);
+    }
+}
