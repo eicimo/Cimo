954254308fa031cabac7ef038b88dc46def72e00
Dicarne <hzdingjiahe@outlook.com>
2017-10-15 15:10:58 +0800

    1、修复了动画播放完东西残留在场上乱七八糟的问题。 2、整理了部分目录。

---
+++ Assets/Script/Fight/BoyBeHurtedControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+using UnityEngine.Playables;
+
+
+public class BoyBeHurtedControl  : BasicPlayableBehaviour
+{
+    public GameObject hitboy;
+
+    //开始播放的时候找到物体
+    public override void OnGraphStart(Playable playable)
+    {
+        hitboy = GameObject.Find("hitBoy");
+    }
+
+    //使物体消失
+    public override void  OnGraphStop(Playable playable)
+    {
+        hitboy.SetActive(false);
+    }
+
+
+
+
+}
---
+++ Assets/Script/Fight/CenterControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenterControl : MonoBehaviour {
+    public GameObject hurtBoy;
+	// Use this for initialization
+	void Start () {
+        hurtBoy.SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+
