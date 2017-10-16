c57844e51721764bb38ece637a2a600469ded856
Dicarne <hzdingjiahe@outlook.com>
2017-10-16 14:17:30 +0800

    增加使用状态机控制动画。 改变了一些目录结构。

---
+++ Assets/Image/Monster/ear/MonsterStatementControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MonsterStatementControl : MonoBehaviour {
+    public Slider sld;
+    public Animator ani;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ani.SetInteger("MonsterScore", (int)sld.value);
+	}
+}
