592213efa8c702ace58b9d4ef8494d581bf0dbbb
Dicarne <hzdingjiahe@outlook.com>
2017-10-16 15:22:45 +0800

    可能解决一个红字问题。

--- Assets/Image/Monster/ear/MonsterStatementControl.cs
+++ Assets/Image/Monster/ear/MonsterStatementControl.cs
@@ -3,22 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MonsterStatementControl : MonoBehaviour {
+public class MonsterStatementControl : MonoBehaviour
+{
     public Slider sld;
     public Slider sld_pla;
     public Animator aniMon;
     public Animator aniPla;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         aniMon.SetInteger("MonsterScore", (int)sld.value);
         aniMon.SetInteger("PlayerScore", (int)sld_pla.value);
         aniPla.SetInteger("MonsterScore", (int)sld.value);
         aniPla.SetInteger("PlayerScore", (int)sld_pla.value);
-        aniPla.SetInteger("diff", (int)(sld.value - sld_pla.value));
+        int diff = (int)(sld.value - sld_pla.value);
+        aniPla.SetInteger("diff", (diff >= 0) ? diff : 0);
     }
 }
