4f1de617c9c38706f0782b8801a428a0adc4fbce
Dicarne <hzdingjiahe@outlook.com>
2017-10-17 14:43:39 +0800

    增加气泡设计

---
+++ Assets/Script/Fight/ScoreControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreControl : MonoBehaviour {
+    public int MonsterScore;
+    public int PlayerScore;
+    public Animator Monster;
+    public Animator Player;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnClickBoom(int id)
+    {
+
+    }
+
+}
