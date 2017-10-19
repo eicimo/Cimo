c4097dc6497e1277408414bf9514873a1c432c1b
Jiahe Ding <hzdingjiahe@gmail.com>
2017-10-19 21:08:08 +0800

    一些错误的修改

--- Assets/Script/Fight/ScoreControl.cs
+++ Assets/Script/Fight/ScoreControl.cs
@@ -13,26 +13,23 @@ public class ScoreControl : MonoBehaviour
     public Animator Player;
 
     public Canvas canvas;
-
-    public void OnClickBoom(PointerEventData id, int t)
-    {
+    
+    public void OnClickBoom(PointerEventData id, int t) {
         var chosen = id.pointerCurrentRaycast.gameObject;
         var text = chosen.GetComponentInChildren<Text>();
         //清除气泡
-        foreach (var item in boom)
-        {
+        foreach (var item in boom) {
             DestroyImmediate(item);
             
         }
 
-        if (text.text == pernouncationList[t])
-        {
-            PlayerScore += 10;
+        if (text.text == pernouncationList[t]) {
+
+            //PlayerScore += 10;
             Debug.Log("t!");
             StartCoroutine(WaitAndRight(t));
         }
-        else
-        {
+        else {
             PlayerScore -= 5;
             Debug.Log("f!");
             StartCoroutine(WaitAndWrong(t));
@@ -40,26 +37,31 @@ public class ScoreControl : MonoBehaviour
 
     }
     public float waitTime = 1;
-    IEnumerator WaitAndRight(int t)
-    {
+    IEnumerator WaitAndRight(int t) {
+        if (t + 1 >= pernouncationList.Count) {
+            PlayerWin = true;
+            PlayerScore += 10;
+            Debug.Log("结束了！");
+            //StopCoroutine("MonsterScoreUp");
+            yield return new WaitForSeconds(waitTime);
+            //StopCoroutine(MonsterScoreUp());
+            //StartTurn();
+            //yield break;
+        }
         yield return new WaitForSeconds(waitTime);
 
         StartBoom(t + 1);
     }
 
-    IEnumerator WaitAndWrong(int t)
-    {
+    IEnumerator WaitAndWrong(int t) {
         yield return new WaitForSeconds(waitTime);
 
         StartBoom(t);
     }
-
+    
     public int PosNum;                                          //选项数目
-    void Awake()
-    {
-
-        for (int i = 0; i < PosNum; i++)
-        {
+    void Awake() {
+        for (int i = 0; i < PosNum; i++) {
             RectTransform t = PosDataObj[i].GetComponent<RectTransform>();
             posData[i] = t.position;        //position世界坐标,rect本地坐标
             qua = t.rotation;
@@ -76,42 +78,47 @@ public class ScoreControl : MonoBehaviour
     public List<Vector3> posData;                              //储存选项位置
     public List<GameObject> boom;                           //气泡的引用
     Quaternion qua;
-    bool MonsterWin = false;
-    bool PlayerWin = false;
+    public bool MonsterWin = false;
+    public bool PlayerWin = false;
 
-    void Start()
-    {
+    void Start() {
         MonsterWin = false;
         PlayerWin = false;
         StartTurn();
     }
     int time = 0;
-    void Update()
-    {
+    void Update() {
         Monster.SetInteger("MonsterScore", MonsterScore);
         Monster.SetInteger("PlayerScore", PlayerScore);
         Player.SetInteger("MonsterScore", MonsterScore);
         Player.SetInteger("PlayerScore", PlayerScore);
-        if(boom.Count>4)
-        {
+        if (boom.Count > 4) {
             Debug.Log("?");
         }
-        if(MonsterWin)
-        {
-            //清除气泡
-            foreach (var item in boom)
-            {
-                DestroyImmediate(item);
 
+        if(PlayerWin) {
+            Debug.Log("PlayerWinNextTurn");
+            PlayerWin = false;
+            StartTurn();
+            
+        }
+
+        if (MonsterWin) {
+            Debug.Log("NextTurn");
+            //清除气泡
+            foreach (var item in boom) {
+                DestroyImmediate(item);
             }
             MonsterWin = false;
-            PlayerWin = false;
-            StartCoroutine(MonsterScoreUp());
-            StartCoroutine(WaitAndNext());
+            StartTurn();
+            
+            ////PlayerWin = false;
+            //StartCoroutine(MonsterScoreUp());
+            //StartCoroutine(WaitAndNext());
         }
+        
     }
-    IEnumerator WaitAndNext()
-    {
+    IEnumerator WaitAndNext() {
         yield return new WaitForSeconds(waitTime);
         StartBoom(time);
         /*
@@ -122,44 +129,47 @@ public class ScoreControl : MonoBehaviour
          * */
     }
     //控制怪物分数自然增长
-    IEnumerator MonsterScoreUp()
-    {
-        for (int i = 0; i < 10; i++)
-        {
-            if (PlayerWin) yield break;
+    IEnumerator MonsterScoreUp() {
+        for (int i = 0; i < 10; i++) {
+            if (PlayerWin) {
+                Debug.Log("MonsterScoreUpEnd");
+                PlayerWin = false;
+                yield break;
+            }
             yield return new WaitForSeconds(1);
+            if (PlayerWin) {
+                Debug.Log("MonsterScoreUpEnd");
+                PlayerWin = false;
+                yield break;
+            }
         }
         MonsterScore += 10;
         MonsterWin = true;
     }
 
     //开始某一回合
-    void StartTurn()
-    {
+    void StartTurn() {
         InitList(0);                                                       //初始化本回合列表
-        StartCoroutine(MonsterScoreUp());
-        StartBoom(time);
+        StartCoroutine(MonsterScoreUp());               //开始一个计算怪物倒计时的协程
+        StartBoom(time);                                           //实例化气泡
 
     }
 
-    void StartBoom(int times)
-    {
-        foreach (var item in boom)
-        {
+
+    void StartBoom(int times) {
+        foreach (var item in boom) {
             DestroyImmediate(item);
 
         }
         boom.Clear();
-        if (times >= pernouncationList.Count)
-        {
-            PlayerWin = true;
-            Debug.Log("结束了！");
+        if (times >= pernouncationList.Count) {
+            //PlayerWin = true;
+            //PlayerScore += 10;
             return;
         }
         GetWrongList(0);                                            //初始化错误列表
         int numid = Random.Range(0, PosNum);
-        for (int i = 0; i < PosNum; i++)
-        {
+        for (int i = 0; i < PosNum; i++) {
             //实例化气泡
             GameObject bom = Instantiate(boomPer, posData[i], qua, canvas.transform);
             Text t = bom.GetComponentInChildren<Text>();
@@ -172,20 +182,18 @@ public class ScoreControl : MonoBehaviour
 
             evt.triggers.Add(trigs);
             boom.Add(bom);
-            if (i == numid)
-            {
+            if (i == numid) {
                 t.text = pernouncationList[times];
             }
-            else
-            {
+            else {
                 t.text = wrongs[times][i];
             }
         }
 
     }
 
-    void InitList(int id)
-    {
+    void InitList(int id) {
+        pernouncationList.Clear();
         meansNow = "交流";
         word = "communication";
         pernouncationList.Add("com");
@@ -195,8 +203,7 @@ public class ScoreControl : MonoBehaviour
         pernouncationList.Add("tion");
     }
 
-    void GetWrongList(int id)
-    {
+    void GetWrongList(int id) {
 
         wrongs.Add(new List<string>
         {
