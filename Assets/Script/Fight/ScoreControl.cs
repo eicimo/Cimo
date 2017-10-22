dcdaa1879666f65a2e24a4f687b69c8d0b17fbab
Jiahe Ding <hzdingjiahe@gmail.com>
2017-10-22 12:09:33 +0800

    增加了xml注释

--- Assets/Script/Fight/ScoreControl.cs
+++ Assets/Script/Fight/ScoreControl.cs
@@ -19,18 +19,23 @@ public class ScoreControl : MonoBehaviour
 
     public Canvas canvas;
     public float emptyWaitTime = 0.5f;
+
+    /// <summary>
+    /// 点击气泡会触发的事件
+    /// </summary>
+    /// <param name="id">点击物的数据</param>
+    /// <param name="t">回合数</param>
     public void OnClickBoom(PointerEventData id, int t) {
         var chosen = id.pointerCurrentRaycast.gameObject;
         var text = chosen.GetComponentInChildren<Text>();
-        //清除气泡
+        // 清除气泡
         foreach (var item in boom) {
 
             if (item != chosen) StartCoroutine(WaitAndDestroy(item));
             else StartCoroutine(WaitAndDestroyMaster(item));
         }
-
+        // 如果回答正确
         if (text.text == pernouncationList[t]) {
-            //PlayerScore += 10;
             Debug.Log("t!");
             StartCoroutine(WaitAndRight(t));
         }
@@ -41,8 +46,15 @@ public class ScoreControl : MonoBehaviour
         }
 
     }
-    public float waitTime = 1;
+
+
+    /// <summary>
+    /// 拼对一个音节的处理函数
+    /// </summary>
+    /// <param name="t">回合数</param>
+    /// <returns></returns>
     IEnumerator WaitAndRight(int t) {
+        // 如果整个单词都拼对了
         if (t + 1 >= pernouncationList.Count) {
             StartBoom(t + 1);
             yield break;
@@ -52,34 +64,55 @@ public class ScoreControl : MonoBehaviour
         StartBoom(t + 1);
     }
 
-    delegate void WaitAndToDo();
-    IEnumerator WaitAndToDoSth(float waitTime, WaitAndToDo w) {
-        yield return new WaitForSeconds(waitTime);
-        w();
-    }
-
+    /// <summary>
+    /// 点击后拼写错误的处理函数
+    /// </summary>
+    /// <param name="t"></param>
+    /// <returns></returns>
     IEnumerator WaitAndWrong(int t) {
         yield return new WaitForSeconds(waitTime + emptyWaitTime);
 
         StartBoom(t);
     }
 
+    /// <summary>
+    /// 等待一段时间后执行无参函数
+    /// </summary>
+    /// <param name="waitTime">等待的时间</param>
+    /// <param name="w">需要执行的无参函数</param>
+    /// <returns></returns>
+    IEnumerator WaitAndToDoSth(float waitTime, JustDo w) {
+        yield return new WaitForSeconds(waitTime);
+        w();
+    }
+
+
+    /// <summary>
+    /// 使气泡缩小并消失
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <returns></returns>
     IEnumerator WaitAndDestroy(GameObject obj) {
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 8; i++) {
             yield return new WaitForSeconds(waitTime / 20);
-            if (obj) obj.transform.localScale -= new Vector3(0.08f, 0.08f, 0.08f);
+            if (obj) obj.transform.localScale -= new Vector3(0.06f, 0.06f, 0);
         }
         DestroyImmediate(obj);
     }
 
+    /// <summary>
+    /// 使选择正确的气泡缩小并消失。
+    /// </summary>
+    /// <param name="obj">气泡</param>
+    /// <returns></returns>
     IEnumerator WaitAndDestroyMaster(GameObject obj) {
         for (int i = 0; i < 10; i++) {
             yield return new WaitForSeconds(waitTime / 20);
-            if (obj) obj.transform.localScale += new Vector3(0.08f, 0.08f, 0.08f);
+            if (obj) obj.transform.localScale += new Vector3(0.02f, 0.02f, 0);
         }
         for (int i = 0; i < 8; i++) {
             yield return new WaitForSeconds(waitTime / 20);
-            if (obj) obj.transform.localScale -= new Vector3(0.1f, 0.1f, 0.1f);
+            if (obj) obj.transform.localScale -= new Vector3(0.05f, 0.05f, 0.05f);
         }
         DestroyImmediate(obj);
     }
@@ -93,19 +126,7 @@ public class ScoreControl : MonoBehaviour
             qua = t.rotation;
         }
     }
-    public int totalCount = 1;                                       //一共几个单词
-    public string meansNow;                                        //当前中文意思
-    public string word;                                                 //当前英文单词
-    public List<string> pernouncationList;                  //音节列表
-    List<List<string>> wrongs = new List<List<string>>();       //错误音节列表
-    public List<string> wrongPernounList;                  //错误音节
-    public GameObject boomPer;                                //气泡预设
-    public List<GameObject> PosDataObj;
-    public List<Vector3> posData;                              //储存选项位置
-    public List<GameObject> boom;                           //气泡的引用
-    Quaternion qua;
-    public bool MonsterWin = false;
-    public bool PlayerWin = false;
+
 
     void Start() {
 
@@ -130,32 +151,15 @@ public class ScoreControl : MonoBehaviour
         if (boom.Count > 4) {
             Debug.Log("?");
         }
-        //if (MonsterWin) {
-        //    //清除气泡
-        //    foreach (var item in boom) {
-        //        DestroyImmediate(item);
 
-        //    }
-        //    MonsterWin = false;
-        //    PlayerWin = false;
-
-        //    StartCoroutine(WaitAndNext());
-        //}
-    }
-    IEnumerator WaitAndNext() {
-        yield return new WaitForSeconds(waitTime);
-        StartTurn();
-        /*
-         * 
-         * 这里因为只有一组数据才选择time，若有多组更改为++time
-         * 
-         * 
-         * */
     }
 
-    public float monsterWaitTime = 8f;
+
     IEnumerator MonsterScoreUpV;
-    //控制怪物分数自然增长
+    /// <summary>
+    /// 控制怪物分数自然增长
+    /// </summary>
+    /// <returns></returns>
     IEnumerator MonsterScoreUp() {
         for (int i = 0; i < 10; i++) {
             if (PlayerWin) yield break;
@@ -174,11 +178,13 @@ public class ScoreControl : MonoBehaviour
         }
 
 
-        StartCoroutine(WaitAndToDoSth(waitTime+emptyWaitTime,StartTurn));
+        StartCoroutine(WaitAndToDoSth(waitTime + emptyWaitTime, StartTurn));
     }
-    public int turn = 0;
-    public int maxTurn = 10;
-    //开始某一回合
+
+
+    /// <summary>
+    /// 开始某一回合。回合数由 time控制。
+    /// </summary>
     void StartTurn() {
         if (turn >= maxTurn) {
             Debug.Log("游戏结束");
@@ -192,16 +198,28 @@ public class ScoreControl : MonoBehaviour
 
 
     }
+
+    /// <summary>
+    /// 用于无参的函数的委托。
+    /// </summary>
     delegate void JustDo();
-    void DeleteAll() {
-        foreach (var item in boom) {
-            if (item) DestroyImmediate(item);
 
-        }
-        boom.Clear();
-    }
+    /// <summary>
+    /// 开始一个音节拼写单元
+    /// </summary>
+    /// <param name="times">回合数</param>
     void StartBoom(int times) {
+        // 立即删除所有气泡
+        JustDo DeleteAll = () =>
+        {
+            foreach (var item in boom) {
+                if (item) DestroyImmediate(item);
 
+            }
+            boom.Clear();
+        };
+
+        // 如果单词拼对了
         if (times >= pernouncationList.Count) {
             PlayerScore += 10;
             PlayerWin = true;
@@ -214,20 +232,25 @@ public class ScoreControl : MonoBehaviour
             return;
         }
         DeleteAll();
-        GetWrongList(0);                                            //初始化错误列表
+
+        //初始化错误列表
+        GetWrongList(0);
         int numid = Random.Range(0, PosNum);
         for (int i = 0; i < PosNum; i++) {
-            //实例化气泡
+            // 实例化气泡
             GameObject bom = Instantiate(boomPer, posData[i], qua, canvas.transform);
             Text t = bom.GetComponentInChildren<Text>();
+
+            // 注册点击事件
             EventTrigger evt = bom.GetComponent<EventTrigger>();
             var trigs = new EventTrigger.Entry
             {
                 eventID = EventTriggerType.PointerClick
             };
             trigs.callback.AddListener((data) => { OnClickBoom((PointerEventData)data, times); });
-
             evt.triggers.Add(trigs);
+
+            // 显示气泡上的文字
             boom.Add(bom);
             if (i == numid) {
                 t.text = pernouncationList[times];
@@ -239,7 +262,12 @@ public class ScoreControl : MonoBehaviour
 
     }
 
+    /// <summary>
+    /// 初始化每一回合的单词内容和音节列表
+    /// </summary>
+    /// <param name="id">回合数</param>
     void InitList(int id) {
+        // TODO —— 根据回合数生成不同的列表
         pernouncationList.Clear();
         meansNow = "交流";
         word = "communication";
@@ -250,6 +278,10 @@ public class ScoreControl : MonoBehaviour
         pernouncationList.Add("tion");
     }
 
+    /// <summary>
+    /// 根据回合数生成与正确音节同时出现的错误音节
+    /// </summary>
+    /// <param name="id">回合数</param>
     void GetWrongList(int id) {
 
         wrongs.Add(new List<string>
@@ -288,4 +320,61 @@ public class ScoreControl : MonoBehaviour
             "qw"
         });
     }
+
+
+    /// <summary>
+    /// 一共几个单词
+    /// </summary>
+    public int totalCount = 1;
+    /// <summary>
+    /// 当前中文意思
+    /// </summary>
+    public string meansNow;
+    /// <summary>
+    /// 当前英文单词
+    /// </summary>
+    public string word;
+    /// <summary>
+    /// 音节列表
+    /// </summary>
+    public List<string> pernouncationList;
+    /// <summary>
+    /// 错误音节列表的列表
+    /// </summary>
+    List<List<string>> wrongs = new List<List<string>>();
+    /// <summary>
+    /// 错误音节列表
+    /// </summary>
+    public List<string> wrongPernounList;
+    /// <summary>
+    /// 气泡预设
+    /// </summary>
+    public GameObject boomPer;
+    /// <summary>
+    /// 气泡位置的标识物体
+    /// </summary>
+    public List<GameObject> PosDataObj;
+    /// <summary>
+    /// 储存选项位置
+    /// </summary>
+    public List<Vector3> posData;
+    /// <summary>
+    /// 气泡的引用
+    /// </summary>
+    public List<GameObject> boom;
+    Quaternion qua;
+    public bool MonsterWin = false;
+    public bool PlayerWin = false;
+
+    /// <summary>
+    /// 第一部分的等待时间
+    /// </summary>
+    public float waitTime = 1;
+
+    public int turn = 0;
+    public int maxTurn = 10;
+    /// <summary>
+    /// 本回合拼写怪物的回答时间（限制）
+    /// </summary>
+    public float monsterWaitTime = 8f;
 }
